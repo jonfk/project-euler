2ae52db9c7c44091a8d4dc735df9db191ab330a2
Jonathan Fok kan <jfokkan@inocybe.com>
2015-02-04 10:17:44 -0500

    FIXED error in 1.rkt off by 1 error below 1000

--- 1/1.rkt
+++ 1/1.rkt
@@ -1,9 +1,10 @@
 #lang racket
 
 (define numbers
-  (for/list ([i 1001]) i))
+  (for/list ([i 1000]) i))
 
 (define matching (filter (lambda (x)
                            (or (equal? (remainder x 3) 0) (equal? (remainder x 5) 0)))
                          numbers))
+(print matching)
 (foldl + 0 matching)
