dcf7bb222797add61ff70a6dc76ad4a9b20c7f1b
Jonathan Fok kan <jfokkan@inocybe.com>
2015-02-10 21:21:49 -0500

    Fixed a few bugs in problem 17 in Go. Still not working

--- 17/17.go
+++ 17/17.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"math"
+	"strconv"
 )
 
 func main() {
@@ -11,12 +12,14 @@ func main() {
 	for i := 1; i <= 1000; i++ {
 		buf.WriteString(num2Str(i))
 	}
+	fmt.Printf("%s", buf.String())
 	fmt.Printf("Result: %d\n", len(buf.String()))
 }
 
 // Provides string for numbers up to 1000 inclusive
 func num2Str(num int) string {
 	numbers := map[int]string{
+		0:    "\nERROR\n",
 		1:    "one",
 		2:    "two",
 		3:    "three",
@@ -29,6 +32,9 @@ func num2Str(num int) string {
 		10:   "ten",
 		11:   "eleven",
 		12:   "twelve",
+		13:   "thirteen",
+		15:   "fifteen",
+		18:   "eighteen",
 		20:   "twenty",
 		30:   "thirty",
 		40:   "forty",
@@ -37,22 +43,34 @@ func num2Str(num int) string {
 		70:   "seventy",
 		80:   "eighty",
 		90:   "ninety",
-		1000: "one thousand",
+		1000: "onethousand",
 	}
 	switch {
 	case numbers[num] != "":
 		return numbers[num]
 	case num > 13 && num < 20:
-		return numbers[digit(num, 1)] + "teen"
-	case num < 100:
-		return numbers[(digit(num, 2)*10)] + numbers[digit(num, 1)]
+		// return numbers[digit(num, 1)] + "teen"
+		return numbers[digitAlt(num, 0)] + "teen"
+	case num > 20 && num < 100:
+		// return numbers[(digit(num, 2)*10)] + numbers[digit(num, 1)]
+		return numbers[(digitAlt(num, 0)*10)] + numbers[digitAlt(num, 1)]
+	case num%100 == 0 && num >= 100:
+		return numbers[digitAlt(num, 0)] + "hundred"
 	case num > 100:
-		return numbers[(num/100)] + "hundredand" + num2Str(num%100)
+		// return numbers[(num/100)] + "hundredand" + num2Str(num%100)
+		return numbers[digitAlt(num, 0)] + "hundredand" + num2Str(num%100)
 	}
-	return ""
+	return "\n!!!ERROR!!!\n"
 }
 
 // Only works for positive integers in base 10
 func digit(number, n int) int {
 	return int((number / int(math.Pow10(n-1))) % 10)
+}
+
+// using string
+func digitAlt(number, n int) int {
+	digit := strconv.Itoa(number)
+	result, _ := strconv.Atoi(string(digit[n]))
+	return result
 }
