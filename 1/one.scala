4283e739288849b2a70be439d27335876499d8bf
Jonathan Fok kan <jfokkan@inocybe.com>
2015-02-13 00:14:15 -0500

    ADDED better solution in scala to problem 1

--- 1/one.scala
+++ 1/one.scala
@@ -5,7 +5,9 @@ import Stream._
 
 object one {
   def main(args : Array[String]) {
-    val result = from(0).take(1000).foldLeft(0){ (acc, x) => if (x % 3 == 0|| x % 5 == 0) { acc + x } else { acc }}
+    // val result = from(0).take(1000).foldLeft(0){ (acc, x) => if (x % 3 == 0|| x % 5 == 0) { acc + x } else { acc }}
+    val result = (from(0).map(_ * 3).takeWhile(_ < 1000) ++ from(0).map(_ * 5).takeWhile(_ < 1000)).distinct.sum
+
     println(result)
   }
 }
