a1bf8c843e3c7397c1c36f5e7f59177102f01947
Jonathan Fok kan <jfokkan@gmail.com>
2015-08-18 21:41:51 -0400

    Feature: ADDED solution to problem 37 in rust

---
+++ 37/37-rust.rs
@@ -0,0 +1,64 @@
+
+fn main() {
+    let truncables = find_truncable_primes().iter().fold(0, |acc, x| acc + x);
+    println!("{:?}", truncables);
+}
+
+fn find_truncable_primes() -> Vec<u64> {
+    let mut truncable = Vec::new();
+    let mut i = 11;
+    let primes = sieve(10000000);
+    while truncable.len() < 11 {
+        let mut truncated = truncate_right(i);
+        truncated.extend(truncate_left(i));
+        if truncated.iter().all(|x| primes.contains(x)) {
+            truncable.push(i);
+            //println!("{:?}", truncable);
+        }
+        println!("{}", i);
+        i += 1;
+    }
+    truncable
+}
+
+fn truncate_right(n: u64) -> Vec<u64> {
+    let mut truncations = Vec::new();
+    let string = n.to_string();
+
+    for i in 1..string.len()+1 {
+        let mut new_string = string.clone();
+        new_string.truncate(i);
+        let num = new_string.parse::<u64>().unwrap();
+        truncations.push(num);
+    }
+    truncations
+}
+
+fn truncate_left(n: u64) -> Vec<u64> {
+    let mut truncations = Vec::new();
+    let string = n.to_string().chars().rev().collect::<String>();
+
+    for i in 1..string.len()+1 {
+        let mut new_string = string.clone();
+        new_string.truncate(i);
+        let num = new_string.chars().rev().collect::<String>().parse::<u64>().unwrap();
+        truncations.push(num);
+    }
+    truncations
+}
+
+fn sieve(bound: u64) -> Vec<u64> {
+    let mut is_prime = vec![true; bound as usize + 1];
+    is_prime[0] = false; is_prime[1] = false;
+    // start from 2
+    for i in 2..bound {
+        if is_prime[i as usize] {
+            let mut j = i + i;
+            while j <= bound {
+                is_prime[j as usize] = false;
+                j += i;
+            }
+        }
+    }
+    is_prime.iter().enumerate().filter_map(|(prime, &is_prime)| if is_prime { Some(prime as u64) } else { None }).collect::<Vec<_>>()
+}
